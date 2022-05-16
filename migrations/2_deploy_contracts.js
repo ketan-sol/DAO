d12976372d128e0cfe750e7b0f7b31d4ba4cc2e1
ketan-sol <ketam@solulab.com>
2022-05-16 18:51:38 +0530

    added governance to deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,4 +11,22 @@ module.exports = async function (deployer) {
   const supply = web3.utils.toWei('1000', 'ether');
 
   await deployer.deploy(MyToken, name, symbol, supply);
+  const token = await MyToken.deployed();
+
+  //send 50 tokens to each voter
+  const amount = web3.utils.toWei('50', 'ether');
+  await token.transfer(voter1, amount, { from: executor });
+  await token.transfer(voter2, amount, { from: executor });
+  await token.transfer(voter3, amount, { from: executor });
+  await token.transfer(voter4, amount, { from: executor });
+  await token.transfer(voter5, amount, { from: executor });
+
+  const minDelay = 1;
+
+  await deployer.deploy(Timelock, minDelay, [proposer], [executor]);
+  const timelock = await Timelock.deployed();
+
+  const quoram = 5; //% of total supply of tokens needed for approval
+  const votingDelay = 0; //number of blocks after proposal until voting becomes active
+  const votingPeriod = 5; //number of blocks voting will stay open
 };
